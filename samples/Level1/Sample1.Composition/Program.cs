e75e2ff8fa3a6a4be0717fee6f9a02c8428b9826
Markus e-Services <msc@schmidt-e-services.de>
2020-07-17 22:32:07 +0200

    NuGet preparation, all samples added

--- samples/Level1/Sample1.Composition/Program.cs
+++ samples/Level1/Sample1.Composition/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Composition;
 using System.Composition.Hosting;
-using System.Diagnostics;
 using System.Reflection;
 using MSPro.CLArgs;
 
@@ -19,38 +17,40 @@ namespace Sample1.Composition
         {
             Arguments arguments = CommandLine.Parse(args);
             Console.WriteLine($"Command-Line: {arguments.CommandLine}");
-            
+
             // Please refer to Microsoft's documentation to get used to System.Composition
             // Basically all classes annotated with the 'Export' attribute
             // will be collected in the serviceContainer.
             var configuration = new ContainerConfiguration().WithAssembly(Assembly.GetExecutingAssembly());
             var serviceContainer = configuration.CreateContainer();
-            
+
             Console.WriteLine(">>> Start Functionality");
             // No error-handling here. We simply take the first Verb and run a command. 
-            var command= serviceContainer.GetExport<IVerbRunner>(arguments.Verbs[0]);
-            command.Run( arguments);
+            var command = serviceContainer.GetExport<IVerbRunner>(arguments.Verbs[0]);
+            command.Run(arguments);
             Console.WriteLine("<<< End Functionality");
         }
     }
 
 
+
     [Export("HelloWorld", typeof(IVerbRunner))]
-    class HelloWorldCommand : IVerbRunner
+    internal class HelloWorldCommand : IVerbRunner
     {
         public void Run(Arguments arguments) => Console.WriteLine("This is my HelloWorld command.");
-    }   
-    
-    
+    }
+
+
+
     [Export("HelloGermany", typeof(IVerbRunner))]
-    class HelloGermanyCommand : IVerbRunner
+    internal class HelloGermanyCommand : IVerbRunner
     {
         public void Run(Arguments arguments) => Console.WriteLine("This is my HelloGermany command.");
     }
-    
-    
-    
-    interface IVerbRunner
+
+
+
+    internal interface IVerbRunner
     {
         void Run(Arguments arguments);
     }
