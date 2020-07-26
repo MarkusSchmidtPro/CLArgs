8d28917ad230d3351a8e1f7f48c37260c5717864
Markus Schmidt <Markus.Schmidt@dell.com>
2020-07-26 10:08:36 +0200

    Sample Options

--- samples/Level1/Sample.Options/Program.cs
+++ samples/Level1/Sample.Options/Program.cs
@@ -6,24 +6,29 @@ using MSPro.CLArgs;
 namespace Level1.Options
 {
     /// <summary>
-    ///     CLArgs example to demonstrate Options basic. 
+    ///     CLArgs example to demonstrate Options basics. 
     /// </summary>
-    /// <remarks>
-    ///     See Properties\launchSettings.json for the used command-line.<br/>
-    /// </remarks>
     internal static class Program
     {
+        /// <summary>
+        ///     The test command-line for this example.
+        /// </summary>
+        private const string COMMAND_LINE = "--target='XML 1' --lines=7";
+
         private static void Main(string[] args)
         {
+            Console.WriteLine(">>> Start Main");
+            Console.WriteLine($"Command-Line: {COMMAND_LINE}");
+            args = COMMAND_LINE.Split(' ');
+            // ------------------------------------------------
+
             Arguments arguments = CommandLineParser.Parse(args);
-            Console.WriteLine(">>> Start Functionality");
-            Console.WriteLine($"Command-Line: {arguments.CommandLine}");
     
             // Check if the 'fileName' option exist - was provided in the command-line
             // E.g. --fileName=.. /fileName:...
             const string FILENAME_TAG = "fileName";
             bool fileNameProvided = arguments.OptionTagProvided(FILENAME_TAG);
-            Console.WriteLine($"Option '{FILENAME_TAG}' was provided in the command-line: {fileNameProvided}");
+            Console.WriteLine($"*** Option '{FILENAME_TAG}' was provided in the command-line: {fileNameProvided}");
             
             // Set default value if not provided
             // Upsert = Update or Insert = Update or Add 
@@ -34,7 +39,9 @@ namespace Level1.Options
             {
                 Console.WriteLine($"Options[{option.Key}] = '{option.Value}'");
             }
-            Console.WriteLine("<<< End Functionality");
+            
+            // ------------------------------------------------
+            Console.WriteLine("<<< End Main");
         }
     }
 }
