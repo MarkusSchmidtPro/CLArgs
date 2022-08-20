e6dd9b2c3a14f51ff98343630f66fe16116e1489
MSPro <Markus@MarkusSchmidt.PRO>
2022-08-21 00:11:21 +0200

    Day end

--- samplesV2/ConsoleApp.NET/Program.cs
+++ samplesV2/ConsoleApp.NET/Program.cs
@@ -1,41 +1,45 @@
-﻿
-using MSPro.CLArgs;
-using MSPro.CLArgs.ListArgs;
+﻿using MSPro.CLArgs;
+
+
 
 // -----------------------------------
 // Simple example how to use CLArgs V2
 // -----------------------------------
+class Program
+{
+    // builder has some functionality that is not used here.
+    // We go with the defaults.
+    public static void Main(string[] args)
+    {
+        var builder = CommandBuilder.Create();
+        Commander2 commander = builder.Build();
+        commander.Execute();
+    }
+}
 
-// builder has some functionality that is not used here.
-// We go with the defaults.
-var builder = CommandBuilder.Create();
-
-Commander2 commander = builder.Build();
-commander.Execute();
 
 
-[Command( "HelloWorld", helpText:"A simple command to print a name in the Console Window.")]
-class HelloWorldCommand : CommandBase2<HelloWorldContext>
+[Command("HelloWorld", helpText: "A simple command to print a name in the Console Window.")]
+internal class HelloWorldCommand : CommandBase2<HelloWorldContext>
 {
     public HelloWorldCommand(IServiceProvider serviceProvider) : base(serviceProvider)
     {
     }
 
-
     protected override void Execute()
     {
-        Console.WriteLine($"Hello {Context.Name}");
+        Console.WriteLine($"Hello {this.Context.Name}");
     }
 }
 
 
 
 /// <summary>
-/// The context defines and describes the arguments supported by a Command.
+///     The context defines and describes the arguments supported by a Command.
 /// </summary>
-class HelloWorldContext
+internal class HelloWorldContext
 {
-    [OptionDescriptor("Name", new[] {"n"},
+    [OptionDescriptor("Name", new[] { "n" },
         Default = "John Doe", Required = false,
         HelpText = "Specify the name of the person to say 'Hello'.")]
     public string? Name { get; set; }
