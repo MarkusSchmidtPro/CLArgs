b064457f7b02f03ece6f829b8421cf7a6838d088
MSPro <Markus@MarkusSchmidt.PRO>
2022-08-24 22:48:55 +0200

    Add missing files

---
+++ samplesV2/Demo06/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using MSPro.CLArgs;
+
+
+
+Console.WriteLine("Demo06 - Context Sets and argument files");
+
+// string[] commandline = "API /url http://localhost /username markus /password schmidt".Split(" ").ToArray();
+
+// Use argument file
+//string[] commandline = "API /p1=p1111 @devsystem.profile".Split(" ").ToArray();
+string[] commandline = "API /?".Split(" ").ToArray();
+
+CommandHostBuilder.Create(commandline).Start();
