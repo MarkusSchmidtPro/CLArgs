1ff1812c44b16079d9ae189572367b8c1fbacbe5
MSPro.NBBoomi <markus@markusschmidt.pro>
2023-12-02 12:39:02 +0100

    Next steps

--- src/MSPro.CLArgs/v2/CommandBase2T.cs
+++ src/MSPro.CLArgs/v2/CommandBase2T.cs
@@ -7,60 +7,45 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace MSPro.CLArgs;
 
-
 [PublicAPI]
-public abstract class CommandWithContext
+public abstract class CommandWithContext(Type contextType)
 {
-    private readonly Type _contextType;
     private ContextPropertyCollection _contextProperties;
 
 
-
-    protected CommandWithContext(Type contextType)
-    {
-        _contextType      = contextType;
-    }
-
-
-
     /// <summary>
-    /// Get all annotated Context properties.
+    ///     Get all annotated Context properties.
     /// </summary>
-    public ContextPropertyCollection ContextProperties 
-        => _contextProperties ??= ContextPropertyCollection.FromType(_contextType);
+    public ContextPropertyCollection ContextProperties
+        => _contextProperties ??= ContextPropertyCollection.FromType(contextType);
 }
 
 
 
-
 [PublicAPI]
-public abstract class CommandBase2<TContext> : CommandWithContext, ICommand2 where TContext : class
+public abstract class CommandBase2<TContext>(IServiceProvider serviceProvider) : CommandWithContext(typeof(TContext)), ICommand2
+    where TContext : class
 {
-    protected readonly IPrinter Print;
-    protected readonly IServiceProvider ServiceProvider;
+    protected readonly IPrinter Print = serviceProvider.GetRequiredService<IPrinter>();
+    protected readonly IServiceProvider ServiceProvider = serviceProvider;
 
-    
-    protected CommandBase2(IServiceProvider serviceProvider) : base(typeof(TContext))
-    {
-        ServiceProvider = serviceProvider;
-        Print           = serviceProvider.GetRequiredService<IPrinter>();
-    }
 
     protected TContext Context { get; private set; }
+
 
 
     void ICommand2.Execute()
     {
         ErrorDetailList errors = new();
 
-        var arguments = ServiceProvider.GetRequiredService<IArgumentCollection>();
-        
+        IArgumentCollection arguments = ServiceProvider.GetRequiredService<IArgumentCollection>();
+
         ContextBuilder contextBuilder = ServiceProvider.GetRequiredService<ContextBuilder>();
-        Context = contextBuilder.Build<TContext>( arguments, ContextProperties, errors);
+        Context = contextBuilder.Build<TContext>(arguments, ContextProperties, errors);
 
         if (!errors.HasErrors())
         {
-            BeforeExecute( errors);
+            BeforeExecute(errors);
             if (!errors.HasErrors())
             {
                 try
@@ -74,34 +59,26 @@ public abstract class CommandBase2<TContext> : CommandWithContext, ICommand2 whe
             }
         }
 
-        if (errors.HasErrors()) OnError(errors, false);
+        AfterExecute(errors);
+    }
+
+
+
+    protected virtual void AfterExecute(ErrorDetailList errors)
+    {
+        switch (errors.Details.Count)
+        {
+            case > 1:
+                throw new AggregateException(errors.Details.Select(
+                    e => new ArgumentException(e.ErrorMessages[0], e.AttributeName)));
+            case > 0:
+                throw new ArgumentException(errors.Details[0].ErrorMessages[0], errors.Details[0].AttributeName);
+        }
     }
 
 
 
     protected abstract void Execute();
-
-
-
-    /// <summary>
-    ///     Error handler in case of any error.
-    /// </summary>
-    /// <remarks>
-    ///     The default implementation of <see cref="OnError" /> simply throws an
-    ///     <see cref="AggregateException" /> in case of any error. You can avoid this by overriding this method.
-    /// </remarks>
-    /// <param name="errors">The errors that have occurred.</param>
-    /// <param name="handled">If <c>true</c> the method does nothing anymore, because it expects the errors have been handled.</param>
-    /// <exception cref="AggregateException">Always</exception>
-    protected virtual void OnError(ErrorDetailList errors, bool handled)
-    {
-        if (handled) return;
-        if (errors.Details.Count > 1)
-            throw new AggregateException(errors.Details.Select(
-                                             e => new ArgumentException(e.ErrorMessages[0], e.AttributeName)));
-
-        throw new ArgumentException(errors.Details[0].ErrorMessages[0], errors.Details[0].AttributeName);
-    }
 
 
 
