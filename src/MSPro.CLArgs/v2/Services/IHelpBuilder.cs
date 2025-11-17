1fc157c3c673ed819d09aa89e34177648968bc64
MSPro <Markus@MarkusSchmidt.Pro>
2025-11-17 18:06:32 +0100

    Start implement new caching

--- src/MSPro.CLArgs/v2/Services/IHelpBuilder.cs
+++ src/MSPro.CLArgs/v2/Services/IHelpBuilder.cs
@@ -14,7 +14,7 @@
         /// </summary>
         /// <remarks>
         ///     This method is called when a VERB was specified and help for this particular verb should be printed.<br />
-        ///     The <see cref="CommandDescriptor">Command's descriptor</see> is passed to the method (the Command is specified by
+        ///     The <see cref="CommandDescriptor2">Command's descriptor</see> is passed to the method (the Command is specified by
         ///     the Verb).
         /// </remarks>
         public string BuildCommandHelp(CommandDescriptor2 c);
